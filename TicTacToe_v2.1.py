63809c087c7592a15615d87e3ac376ab3811d33b
Josh <Joshualawson8@gmail.com>
2018-10-02 18:14:10 -0700

    Changed Tic-Tac-Toe to Tic-Tac-Tic-Tac-Toe. (5x5)

--- TicTacToe_v2.1.py
+++ TicTacToe_v2.1.py
@@ -6,7 +6,7 @@ Spyder Editor
 CURRENT VERSION (2.1):
     Player vs player
     Player vs AI making random moves
-    
+
 TODO:
     Check invalid input when entering row/column
     Randomly choose which player goes first
@@ -21,28 +21,28 @@ turn = 1
 p1 = 1
 p2 = 2
 resp = ""
-markers = [[" "," "," "],[" "," "," "],[" "," "," "]]
+markers = [[" "," "," "," "," "],[" "," "," "," "," "],[" "," "," "," "," "],[" "," "," "," "," "],[" "," "," "," "," "],]
 
 # Prints current board
 def printBoard():
-    print("    1   2   3")
-    print("   -----------")
-    for x in range(0, 3):
+    print("    1   2   3   4   5")
+    print("   ------------------")
+    for x in range(0, 5):
         print(str(x + 1) + " |", end=" ")
-        for y in range(0, 3):
+        for y in range(0, 5):
             print(str(markers[x][y]) + " | ", end="")
         print()
-        print("   -----------")
+        print("   -------------------")
     return
 
 # Asks for player's move and changes marker
 def move(player):
     row = -1
     col = -1
-    
+
     print()
     print()
-    while((row < 1) or (row > 3) or (col < 1) or (col > 3) or (markers[row-1][col-1] != " ")):
+    while((row < 1) or (row > 5) or (col < 1) or (col > 5) or (markers[row-1][col-1] != " ")):
         print("Enter move for player \"" + str(player) + "\":")
         print()
         print("Enter row.", end=" ")
@@ -56,14 +56,13 @@ def move(player):
 
 # Chooses a random legal move
 def randomAIMove():
-    open = [0,1,2,3,4,5,6,7,8]
-    for x in range(0,3):
-        for y in range(0,3):
-            if markers[x][y] != " ":
-                loc = 3 * x + y
-                open.remove(loc)
-    item =  random.randint(0,len(open))
-    markers[int(open[item] / 3)][int(open[item] % 3)] = "O"
+    x =  random.randint(0,4)
+    y =  random.randint(0,4)
+    while (markers[x][y] == "O" or markers[x][y] == "X"):
+        x =  random.randint(0,4)
+        y =  random.randint(0,4)
+
+    markers[x][y] = "O"
     return
 
 # Checks if a player has won or if there is a tie
@@ -71,22 +70,22 @@ def checkEnd():
     printBoard()
     global gameOver
     gameOver = 2
-    for x in range(0, 3):
-        if markers[x][0] == markers[x][1] == markers[x][2] and markers[x][0] != " ":
+    for x in range(0, 5):
+        if markers[x][0] == markers[x][1] == markers[x][2] == markers[x][3] == markers[x][4] and markers[x][0] != " ":
             print(str(markers[x][0]) + " has won!")
             gameOver = 1
-        elif markers[0][x] == markers[1][x] == markers[2][x] and markers[0][x] != " ":
+        elif markers[0][x] == markers[1][x] == markers[2][x] == markers[3][x] == markers[4][x] and markers[0][x] != " ":
             print(str(markers[0][x] + " has won!"))
             gameOver = 1
-    if markers[0][0] == markers[1][1] == markers[2][2] and markers[0][0] != " ":
+    if markers[0][0] == markers[1][1] == markers[2][2] == markers[3][3] == markers[4][4] and markers[0][0] != " ":
         print(str(markers[0][0]) + " has won!")
         gameOver = 1
-    elif markers[0][2] == markers[1][1] == markers[2][0] and markers[0][2] != " ":
+    elif markers[0][4] == markers[1][3] == markers[2][2] == markers[3][1] == markers[4][0] and markers[0][4] != " ":
         print(str(markers[0][2]) + " has won!")
         gameOver = 1
     if gameOver != 1:
-        for x in range(0, 3):
-            for y in range(0, 3):
+        for x in range(0, 5):
+            for y in range(0, 5):
                 if str(markers[x][y]) == " ":
                     gameOver = 0
     if gameOver == 2:
@@ -94,7 +93,7 @@ def checkEnd():
     elif gameOver == -1:
         return
     return
-    
+
 # Cycles between turns
 def nextTurn():
     global turn
@@ -124,7 +123,7 @@ while resp != "ai" and resp != "pvp":
 if resp == "ai":
     p1 = 1
     p2 = 0
-    
+
 elif resp == "pvp":
     p1 = 1
     p2 = 2
